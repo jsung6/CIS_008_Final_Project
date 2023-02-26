9eb541f18ba3339ce7a00c5169f5331d6fc22a11
jsung6 <108384900+jsung6@users.noreply.github.com>
2023-02-25 18:35:03 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Medical Store Management System - CIS 008 Final Project
+
+Medical Store Management System for small enterprises based on Python. UI created to provide electronic billing documentation, database access, and stock maintenance with valued customer assistance.
+These attributes have now been employed to calcualte consumer discounts, daily revenues, and procedures to avoid possible income loss.
+
+- GUI?
+- Login/Logout
+- Dedicated feature for stock maintenance
+- Feature to access the database
+- Various options to generate bills and handle cash
+"""
